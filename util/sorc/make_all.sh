d2d486b4d59b115dc59258f46c74aaa6751e661d
Eric Rogers 301-763-8000x7227 (devonprod) wx20er <Eric.Rogers@v71a2.ncep.noaa.gov>
2019-06-20 15:05:16 +0000

    add custom version of wgrib2 with MCONV instead of MDIV

--- util/sorc/make_all.sh
+++ util/sorc/make_all.sh
@@ -16,6 +16,11 @@ export OUTDIR=${OUTmain}/${MACHID}.exec
 
 make -f ./Makefile
 
+cd wgrib2_v2.0.7a/sorc
+./install_all_grib_util_wcoss.sh_wgrib2only
+cd ../exec
+cp wgrib2 ../../../exec/dell.exec/.
+
 set +x
 module unload build/v4.0.0_build
 set -x
